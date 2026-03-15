91f99b5005680a3290179310b4bf811b67756941
archi2305 <archisnehi97@gmail.com>
2026-03-15 23:16:53 +0530

    Added VendorProductMapping model

--- vendor_product_mapping/models.py
+++ vendor_product_mapping/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from vendor.models import Vendor
+from product.models import Product
 
-# Create your models here.
+
+class VendorProductMapping(models.Model):
+
+    vendor = models.ForeignKey(
+        Vendor,
+        on_delete=models.CASCADE,
+        related_name="vendor_products"
+    )
+
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.CASCADE,
+        related_name="product_vendors"
+    )
+
+    primary_mapping = models.BooleanField(default=False)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ['vendor', 'product']
+
+    def __str__(self):
+        return f"{self.vendor.name} -> {self.product.name}"
